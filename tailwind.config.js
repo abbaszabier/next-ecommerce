3d134e65467ae8d18540487c0f2b2c5de4efeca6
abbaszabier <abbas.zabier06@gmail.com>
2024-08-10 21:32:12 +0700

    migrate js to ts

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+        pop: ["Poppins", "sans-serif"],
+      },
     },
   },
   plugins: [],
